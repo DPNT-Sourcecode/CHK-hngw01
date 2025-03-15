2f6e11e12d43c5e88d5b52be364737773b7b99b7
sbx_user1051 <sbx_user1051@169.254.4.157>
2025-03-15 16:29:58 +0000

    Sat Mar 15 16:29:58 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -45,7 +45,7 @@ namespace BeFaster.App.Solutions.CHK
             prices.Add('W',20);
             prices.Add('X',17);
             prices.Add('Y',20);
-            prices.Add('Z',21);
+            prices.Add('Z',20);
 
             for(int i = 0; i<skus.Length; i++)
             {
@@ -68,7 +68,7 @@ namespace BeFaster.App.Solutions.CHK
                     {
                         FreeQ +=1;
                     }
-                } 
+                }  
                 else{ 
                     Amount.Add(skus[i], 1);
                 }  
@@ -188,6 +188,7 @@ namespace BeFaster.App.Solutions.CHK
 }
     }
 }
+
 
 
 
