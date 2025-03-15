68cc7313b6ca4b64caf955d54ff17e46df11547d
sbx_user1051 <sbx_user1051@169.254.4.157>
2025-03-15 13:09:28 +0000

    Sat Mar 15 13:09:28 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -9,6 +9,7 @@ namespace BeFaster.App.Solutions.CHK
             string [] items = skus.Split(',');
             Dictionary<string,int> prices = new Dictionary<string, int>();
             Dictionary<string,int> Amount = new Dictionary<string, int>();
+            int Price = 0;
 
             prices.Add("A",50);
             prices.Add("B",30);
@@ -17,9 +18,33 @@ namespace BeFaster.App.Solutions.CHK
 
             for(int i = 0; i<items.Length; i++)
             {
-                if()
+                if(Amount.ContainsKey(items[i]))
+                {
+                    Amount[items[i]] ++;
+                    if(items[i] == "A" && Amount[items[i]] % 3 == 0)
+                    {
+                        Price = Price + 30;
+                    }
+                    else{
+                       Price = Price + prices[items[i]]; 
+                    }
+                    if(items[i] == "B" && Amount[items[i]] % 2 == 0)
+                    {
+                        Price = Price + 15;
+                    }
+                                        else{
+                       Price = Price + prices[items[i]]; 
+                    }
+
+                }
+                else{
+                    Amount.Add(items[i], 1);
+                    Price = Price + prices[items[i]];
+                }
             }
+            return Price;
 
         }
     }
 }
+
