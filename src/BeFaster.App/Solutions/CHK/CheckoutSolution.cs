1b991fc50f6f55a99dd61d9bcdc5bd28001c4ed1
sbx_user1051 <sbx_user1051@169.254.4.157>
2025-03-15 12:55:28 +0000

    Sat Mar 15 12:55:28 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,7 +6,21 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string? skus)
         {
-            throw new SolutionNotImplementedException();
+            string [] items = skus.Split(',');
+            Dictionary<string,int> prices = new Dictionary<string, int>();
+            Dictionary<string,int> Amount = new Dictionary<string, int>();
+
+            prices.Add("A",50);
+            prices.Add("B",30);
+            prices.Add("C",20);
+            prices.Add("D",15);
+
+            for(int i = 0; i<items.Length; i++)
+            {
+                if()
+            }
+
         }
     }
 }
+
